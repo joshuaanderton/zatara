026abfc0a8129538cc11f79d85926fb331719d74
Joshua Anderton <info@joshanderton.com>
2024-03-18 21:32:41 -0700

    Skip auto-tets for now

--- tests/Zatara/AppActionTest.php
+++ tests/Zatara/AppActionTest.php
@@ -8,32 +8,32 @@ use Zatara\Support\Facades\Zatara;
 
 uses(TestCase::class, RefreshDatabase::class)->in(__DIR__);
 
-it('Actions without auth middleware run without error and return view or redirect', function () {
-    $actions = Zatara::getActions()->filter(fn ($action) => ! in_array('auth', $action['action']['middleware']));
-    $actions->each(function ($action) {
-        $method = $action['methods'][0];
-        $parameters = [];
+// it('Actions without auth middleware run without error and return view or redirect', function () {
+//     $actions = Zatara::getActions()->filter(fn ($action) => ! in_array('auth', $action['action']['middleware']));
+//     $actions->each(function ($action) {
+//         $method = $action['methods'][0];
+//         $parameters = [];
 
-        $response = $this->$method(
-            route($action['action']['as'], $parameters)
-        );
+//         $response = $this->$method(
+//             route($action['action']['as'], $parameters)
+//         );
 
-        expect(in_array($response->status(), [302, 200]))->toBe(true);
-    });
-});
+//         expect(in_array($response->status(), [302, 200]))->toBe(true);
+//     });
+// });
 
-it('Actions with auth middleware run without error and return view or redirect', function () {
-    $actions = Zatara::getActions()->filter(fn ($action) => in_array('auth', $action['action']['middleware']));
-    $this->actingAs($user = User::factory()->withPersonalTeam()->create());
+// it('Actions with auth middleware run without error and return view or redirect', function () {
+//     $actions = Zatara::getActions()->filter(fn ($action) => in_array('auth', $action['action']['middleware']));
+//     $this->actingAs($user = User::factory()->withPersonalTeam()->create());
 
-    $actions->each(function ($action) {
-        $method = $action['methods'][0];
-        $parameters = [];
+//     $actions->each(function ($action) {
+//         $method = $action['methods'][0];
+//         $parameters = [];
 
-        $response = $this->$method(
-            route($action['action']['as'], $parameters)
-        );
+//         $response = $this->$method(
+//             route($action['action']['as'], $parameters)
+//         );
 
-        expect(in_array($response->status(), [302, 200]))->toBe(true);
-    });
-});
+//         expect(in_array($response->status(), [302, 200]))->toBe(true);
+//     });
+// });
